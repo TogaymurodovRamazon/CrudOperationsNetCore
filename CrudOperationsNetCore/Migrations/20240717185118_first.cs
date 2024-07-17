ac85db9f2e934571b2c1de358f290dd1bc9a37c5
Ramazon Tog'aymurodov <togaymurodovramazon179@gmail.com>
2024-07-18 01:09:34 +0500

    Add project files.

---
+++ CrudOperationsNetCore/Migrations/20240717185118_first.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudOperationsNetCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InActive",
+                table: "Brands",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Brands",
+                newName: "InActive");
+        }
+    }
+}
---
+++ CrudOperationsNetCore/Models/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsNetCore.Models
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
